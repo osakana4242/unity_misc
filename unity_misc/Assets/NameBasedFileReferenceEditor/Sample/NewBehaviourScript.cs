fd6b3a065316262b10935b1d2676558cbfaa52c3
osakana4242 <osakana4242@gmail.com>
2024-02-24 21:46:55 +0900

    feat: 不要ファイル除去

--- unity_misc/Assets/NameBasedFileReferenceEditor/Sample/NewBehaviourScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace NameBasedFileReferenceEditors.Samples {
-	public class NewBehaviourScript : MonoBehaviour {
-		public GameObject[] gameObjects;
-	}
-}
