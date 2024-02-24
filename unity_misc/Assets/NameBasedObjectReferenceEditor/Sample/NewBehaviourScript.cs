aa8deb282f87a19615b945d7f92da487b79922ac
osakana4242 <osakana4242@gmail.com>
2024-02-24 11:44:36 +0900

    feat: sample

---
+++ unity_misc/Assets/NameBasedObjectReferenceEditor/Sample/NewBehaviourScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NameBasedObjectReferenceEditors.Samples {
+	public class NewBehaviourScript : MonoBehaviour {
+		public GameObject[] gameObjects;
+	}
+}
